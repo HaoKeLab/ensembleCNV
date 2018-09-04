8033eae5b4a2697429f8eb5db5f615fefcaabad7
Zhongyang Zhang <zhangzhongyang06@gmail.com>
2018-09-04 16:40:48 -0400

    update

---
+++ example/example_create_CNVR/test_create_CNVR.sh
@@ -0,0 +1,17 @@
+## path to the directory in which all ensembleCNV scripts and data are located. 
+path_ensembleCNV=""
+
+Rscript ${path_ensembleCNV}/03_create_CNVR/step.1.CNV.data.R \
+${path_ensembleCNV}/example/example_create_CNVR/results/ \
+${path_ensembleCNV}/example/example_create_CNVR/data/iPattern_cnvr1_all_calls.txt \
+${path_ensembleCNV}/example/example_create_CNVR/data/PennCNV_cnvr1.txt \
+${path_ensembleCNV}/example/example_create_CNVR/data/QuantiSNP_cnvr1.txt \
+${path_ensembleCNV}/example/example_create_CNVR/data/Sample_Map.txt
+
+Rscript ${path_ensembleCNV}/03_create_CNVR/step.2.create.CNVR.R \
+--icnv ${path_ensembleCNV}/example/example_create_CNVR/results/cnv.ipattern.txt \
+--pcnv ${path_ensembleCNV}/example/example_create_CNVR/results/cnv.penncnv.txt \
+--qcnv ${path_ensembleCNV}/example/example_create_CNVR/results/cnv.quantisnp.txt \
+--snp ${path_ensembleCNV}/example/example_create_CNVR/data/SNP.chr1.pfb \
+--centromere ${path_ensembleCNV}/example/example_create_CNVR/data/centromere_hg19.txt \
+--output ${path_ensembleCNV}/example/example_create_CNVR/results/
