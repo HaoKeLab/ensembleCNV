ea9850e8aaa25b981a503434c873862883ccfb81
Zhongyang Zhang <zhangzhongyang06@gmail.com>
2018-09-19 17:02:33 -0400

    update

---
+++ example/example_boundary_refinement/test_boundary_refinement.sh
@@ -0,0 +1,21 @@
+## path to the directory in which all ensembleCNV scripts and data are located. 
+path_ensembleCNV=""
+
+Rscript ${path_ensembleCNV}/05_boundary_refinement/CNVR.boundary.refinement.R \
+--chr 2 \
+--datapath ${path_ensembleCNV}/example/example_boundary_refinement/data/ \
+--matrixpath ${path_ensembleCNV}/example/example_boundary_refinement/matrix/ \
+--resultpath ${path_ensembleCNV}/example/example_boundary_refinement/results/ \
+--rcppfile ${path_ensembleCNV}/05_boundary_refinement/refine.cpp \
+--centromere ${path_ensembleCNV}/example/example_boundary_refinement/data/centromere_hg19.txt \
+--plot
+
+
+
+
+
+
+
+
+
+
