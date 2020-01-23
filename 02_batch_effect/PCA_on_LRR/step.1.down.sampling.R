19592afada227a5557727813dada6674e7b0afdb
Zhongyang Zhang <zhangzhongyang06@gmail.com>
2020-01-23 10:29:30 -0500

    Update step.1.down.sampling.R
    correction

--- 02_batch_effect/PCA_on_LRR/step.1.down.sampling.R
+++ 02_batch_effect/PCA_on_LRR/step.1.down.sampling.R
@@ -15,7 +15,7 @@ path_output <- args[2] ## path to save randomly selected SNPs
 ## sampleing from chr: 1-22
 dat_snps <- fread( input = file_snps )
 dat_snps <- as.data.frame(dat_snps, stringsAsFactors = FALSE)
-dat_snps <- subset(dat_snps, Chromosome %in% 1:22)
+dat_snps <- subset(dat_snps, Chr %in% 1:22)
 
 snps <- sample( dat_snps$Name )
 snps.selected <- snps[ 1:100000 ]
